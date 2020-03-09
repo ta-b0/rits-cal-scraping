3f7c8989ecbc97a6f85d2e6c2f5caf12c8e0cf1c
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-03-09 18:57:02 +0900

    feat: ical形式で出力させた！

--- rits-cal-scraping.py
+++ rits-cal-scraping.py
@@ -2,6 +2,9 @@ import pandas as pd
 import datetime
 import csv
 from icalendar import Calendar, Event
+from datetime import datetime
+import pytz
+
 def read_cal():
     url = "http://www.ritsumei.ac.jp/profile/info/calendar/2020/"
     data = pd.read_html(url,header = 0)
@@ -9,6 +12,7 @@ def read_cal():
         df = data[semester]
         # print(df)
         parse_cal(df)
+        # parse_cal_in_ics(df)
 
 def parse_cal(df):
     tocsv = [["Subject","Start Date","End Date","Description","Location"]]
@@ -38,6 +42,37 @@ def parse_cal(df):
         writer = csv.writer(f)
         writer.writerows(tocsv)
 
+# ics形式で出力させる
+def parse_cal_in_ics(df):
+    cal = Calendar()
+    for i in range(len(df)):
+        event = Event()
+        # 日付の処理
+        month = df.at[i,'月']
+        day = df.at[i,'日']
+        summary = df.at[i,'行事']
+        if month >= 4:
+            year = 2020
+        elif month == 3 and day == 31 and i == 0:
+            year = 2020
+        else:
+            year = 2021
+        month = '%02d' % int(month)
+        day = '%02d' % int(day)
+        # 開始時間と終了時間を同じ日にしている，これで終日になる??
+        event.add('dtstart', datetime(int(year), int(month), int(day), tzinfo=pytz.timezone("Asia/Tokyo")))
+        event.add('dtend', datetime(int(year), int(month), int(day), tzinfo=pytz.timezone("Asia/Tokyo")))
+        event.add('summary', summary)
+        event.add('location', "立命館大学")
+        cal.add_component(event)
+
+    with open("cal.ics","a") as f:
+        print(display(cal))
+        f.write(display(cal))
+
+# icalをデコードして表示する用にするfunction
+def display(cal):
+    return cal.to_ical().decode('utf-8').replace('\r\n', '\n').strip()
 def main():
     read_cal()
 
